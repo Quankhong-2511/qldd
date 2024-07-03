e9641595a2c3b90c1c54ef5aba977688149916be
Khong Quan <khongquan354@gmail.com>
2024-07-03 16:00:01 +0700

    first commit

---
+++ src/database/migrations/1719995233173-AddTableStudent.ts
@@ -0,0 +1,11 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class AddTableStudent1719995233173 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
